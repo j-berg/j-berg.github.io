89f3ea3ea13b275a634799234c1f0e07479200b5
Jordan Berg <j-berg@users.noreply.github.com>
2020-11-01 17:59:51 -0700

    Update

--- blog/blog_code/2020-02-12_superspreading/tuberculosis_hold.js
+++ blog/blog_code/2020-02-12_superspreading/tuberculosis_hold.js
@@ -1,6 +1,6 @@
 d3.json('blog/blog_code/2020-02-12_superspreading/tuberculosis_network.json').then(data=>{
 
-  var width = window.innerWidth * 0.7;
+  var width = 500;
   var height = window.innerHeight * 0.8;
   var start = 90;
   var end = 160;
